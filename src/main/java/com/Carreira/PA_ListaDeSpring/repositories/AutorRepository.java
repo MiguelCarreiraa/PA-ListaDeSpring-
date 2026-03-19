8642cd10b66a1fa6ad670dbfce53134d9dbe0251
MiguelCarreiraa <miguelitocarreira@hotmail.com>
2026-03-19 14:21:19 -0300

    Autor Repositoty

---
+++ src/main/java/com/Carreira/PA_ListaDeSpring/repositories/AutorRepository.java
@@ -0,0 +1,8 @@
+package com.Carreira.PA_ListaDeSpring.repositories;
+
+import com.Carreira.PA_ListaDeSpring.models.AutorModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutorRepository extends JpaRepository<AutorModel, Long> {
+}
+
