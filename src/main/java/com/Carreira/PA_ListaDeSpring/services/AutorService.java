0d2e9325b8b72ee9c650df6e466ad287efa933c1
MiguelCarreiraa <miguelitocarreira@hotmail.com>
2026-03-19 14:22:25 -0300

    Autor Service

---
+++ src/main/java/com/Carreira/PA_ListaDeSpring/services/AutorService.java
@@ -0,0 +1,43 @@
+package com.Carreira.PA_ListaDeSpring.services;
+
+import com.Carreira.PA_ListaDeSpring.models.AutorModel;
+import com.Carreira.PA_ListaDeSpring.repositories.AutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AutorService {
+
+    @Autowired
+    private AutorRepository autorRepository;
+
+    public List<AutorModel> findAll(){
+        return autorRepository.findAll();
+    }
+
+    public AutorModel criarAutor(AutorModel autor){
+        return autorRepository.save(autor);
+    }
+
+    public AutorModel buscarAutorPorId(Long id){
+        return autorRepository.findById(id).get();
+    }
+
+    public AutorModel atualizarAutor(Long id, AutorModel autorModel){
+        AutorModel model =  autorRepository.findById(id).get();
+        model.setNome(autorModel.getNome());
+        model.setDataNascimento(autorModel.getDataNascimento());
+        model.setNacionalidade(autorModel.getNacionalidade());
+        return autorRepository.save(model);
+    }
+
+
+    public void deletarAutor(Long id){
+        autorRepository.deleteById(id);
+    }
+
+}
+
