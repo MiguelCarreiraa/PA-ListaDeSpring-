26d2e61b1207de0a5f97ed9bc322d497d9a108cf
MiguelCarreiraa <miguelitocarreira@hotmail.com>
2026-03-19 14:23:50 -0300

    Autor Controller Crud Completo

---
+++ src/main/java/com/Carreira/PA_ListaDeSpring/controllers/AutorController.java
@@ -0,0 +1,43 @@
+package com.Carreira.PA_ListaDeSpring.controllers;
+
+
+import com.Carreira.PA_ListaDeSpring.models.AutorModel;
+import com.Carreira.PA_ListaDeSpring.services.AutorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/autores")
+public class AutorController {
+
+    @Autowired
+    private AutorService autorService;
+
+    @GetMapping
+    public List<AutorModel> listarAutores(){
+        return autorService.findAll();
+    }
+
+    @PostMapping
+    public AutorModel criarAutor(@RequestBody AutorModel autorModel){
+        return autorService.criarAutor(autorModel);
+    }
+
+    @GetMapping("/{id}")
+    public AutorModel buscarAutor(@PathVariable Long id){
+        return autorService.buscarAutorPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public AutorModel atualizarAutor(@PathVariable Long id, @RequestBody AutorModel autorModel){
+        return autorService.atualizarAutor(id, autorModel);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarAutor(@PathVariable Long id){
+        autorService.deletarAutor(id);
+    }
+}
+
